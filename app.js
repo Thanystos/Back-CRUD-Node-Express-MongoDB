03ba42d44bbcbde5ea216356b4893c08e294aac9
Thanystos <guichardanthony@live.fr>
2022-10-10 04:02:31 +0200

    Création d'une route POST et récupération des données entrées

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // Permet d'intercepter toute réponse json envoyée
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
-app.use('/api/stuff', (req, res, next) => {
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body); // Possible grâce à la ligne 5
+    res.status(201).json({
+        message: 'objet créé'
+    });
+});
+
+app.get('/api/stuff', (req, res, next) => {
     const stuff = [
       {
         _id: 'oeihfzeoi',
