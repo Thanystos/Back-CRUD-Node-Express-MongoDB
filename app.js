f563a4f22cd5bce2a5a60f547cf845ffde95b913
Thanystos <guichardanthony@live.fr>
2022-10-10 04:25:29 +0200

    Ajout de mongoose et connexion à ma bdd MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://Thanystos:Devilplop0@coursocnodeexpressmongo.2cvjeus.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json()); // Permet d'intercepter toute réponse json envoyée
 
