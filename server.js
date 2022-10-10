8bcf14f497a1e9a3976d9cd255ae732e3407fbf8
Thanystos <guichardanthony@live.fr>
2022-10-10 03:26:33 +0200

    Installation d'express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('voila la réponse du serveur');
+    res.end('voila la réponse du  serveur');
 });
 
 server.listen(process.env.PORT || 3000);
